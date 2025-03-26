c2b3f78fc174defe5bc43e247a291e49467aaa24
felix <felix@tractiondk.com>
2025-03-26 16:10:36 -0400

    Final updates for release, added new assets and updated code for Android/iOS

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.felixservice.salonapprentissage"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "3.0.0"
+        versionCode = 4
+        versionName = "4.0.0"
     }
 
     signingConfigs {
