c1606a8ca06fece45e66023a3f0610a364436873
felix <felix@tractiondk.com>
2025-09-28 10:23:35 -0400

    Increment to version 3.0.6 (build 1) - fresh start after fixing iOS binary issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.felixservice.salonapprentissage"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 17
-        versionName = "3.0.5"
+        versionCode = 1
+        versionName = "3.0.6"
     }
 
     signingConfigs {
