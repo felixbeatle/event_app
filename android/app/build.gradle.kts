573a3089860398a565a271adc1153812f3ff0c96
felix <felix@tractiondk.com>
2025-09-29 18:56:25 -0400

    Increment to version 3.0.7 (build 1) - new release after 3.0.6 publication

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.felixservice.salonapprentissage"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 7
-        versionName = "3.0.6"
+        versionCode = 1
+        versionName = "3.0.7"
     }
 
     signingConfigs {
