7aa0826edd1f24f3c83137ffe3116101e0511f9d
felix <felix@tractiondk.com>
2025-09-29 18:45:37 -0400

    Clean Info.plist - Remove embedded privacy manifest, use package_info_plus built-in - v3.0.6(7)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "com.felixservice.salonapprentissage"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 5
+        versionCode = 7
         versionName = "3.0.6"
     }
 
