415bf4db555f05a3c318638d88db7f68c4682539
felix <felix@tractiondk.com>
2025-10-03 15:42:56 -0400

    Version 3.0.9 (build 1) - New iOS release with portrait orientation support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.felixservice.salonapprentissage"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "3.0.8"
+        versionCode = 1
+        versionName = "3.0.9"
     }
 
     signingConfigs {
